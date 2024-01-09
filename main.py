b01b02f945d29f54afb36241f81e2480b7e777b2
Ketan Solanki <ketangsolanki129@gmail.com>
2024-01-09 12:15:38 +0530

    Minor changes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import datetime as dt
 from datetime import datetime, timedelta
 import json
-from fastapi import FastAPI, Depends, HTTPException, status
-from jose import jwt, JWTError
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
 from modules.event.routes import event_router
 from modules.db.routes import db_router
-from modules.auth.auth import get_current_user, login_for_access_token
+from modules.auth.auth import login_for_access_token
 
 
 app = FastAPI()
--- modules/event/event.py
+++ modules/event/event.py
@@ -3,7 +3,6 @@ import datetime as dt
 from datetime import datetime, timedelta
 import json
 
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow

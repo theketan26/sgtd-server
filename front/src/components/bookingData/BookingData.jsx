014eaa5e67f46627fc2bd7f396c749b643f2812a
Ketan Solanki <ketangsolanki129@gmail.com>
2024-01-10 18:38:28 +0530

    Booking details

--- front/src/components/bookingData/BookingData.jsx
+++ front/src/components/bookingData/BookingData.jsx
@@ -2,23 +2,31 @@ import { useEffect, useState } from "react"
 
 export default function (props) {
     const [isBook, setIsBook] = useState(false);
+    const [data, setData] = useState([]);
 
 
-    useEffect(() => {
+    async function handleChange() {
         try {
             if (props.data['message'].length > 0) {
-                setIsBook(true);
+                await setIsBook(true);
+                await setData(props.data['message']);
             }
             else {
-                setIsBook(false);
+                await setIsBook(false);
+                await setData([]);
             }
         } catch (e) {
         }
+    }
+
+
+    useEffect(() => {
+        handleChange();
     }, [props])
 
 
     return (
-        <div className = "self-center mt-10 p-5 text-xl font-bold">
+        <div className = "self-center mt-5 p-5 text-xl font-bold">
             { props.isLoading ? 
                 <div>
                     Loading
@@ -28,7 +36,28 @@ export default function (props) {
                     { isBook ? 
                         <div className = "flex flex-col align-center">
                             <div className = "bg-rose-300 text-center">Booked</div>
-                            <button className = "mt-5 px-3 py-1 border-4">Check</button>
+                            {
+                                data.map((item, index) => {
+                                    return (
+                                        <div key = { index } className = "mt-5">
+                                            <div className = "text-center font-medium">
+                                                <div className = "">
+                                                    { item.summary }
+                                                </div>
+                                                <div>
+                                                    <div>Host: { item.description.host_name }</div>
+                                                    <div>Number: { item.description.host_number }</div>
+                                                    <div>Email: { item.description.host_email }</div>
+                                                    <div>Address: { item.description.host_address }</div>
+                                                    <div>Address: { item.description.host_address }</div>
+                                                    <div>Booker: { item.description.booker_name }</div>
+                                                    <div>Booker Number: { item.description.booker_number }</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                         :
                         <div className = "bg-green-300">

b56410ed5801a3fce4260227505d4f1e123a1f55
Ketan Solanki <ketangsolanki129@gmail.com>
2024-01-11 16:48:03 +0530

    Auth routing updated

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
+import { login, logout } from './actions/actions';
+import store from './store';
 
 
 import Home from './home/Home';
@@ -8,11 +13,34 @@ import Login from './login/Login';
 import Register from './register/Register';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-	<React.StrictMode >
-		{/* <Home /> */}
-		<Login />
-		{/* <Register />  */}
-	</ React.StrictMode>
+const App = () => {
+    const isLoggedIn = useSelector((state) => state.isLoggedIn);
+	const dispatch = useDispatch();
+
+
+	return (
+			<Router>
+				<Routes>
+					{ isLoggedIn ? (
+							<></>
+						) : (
+							<>
+								<Route path="/login" element = { <Login /> } />
+								<Route path="/register" element = { <Register /> } />
+							</>
+					)}
+					<Route path="/" element = { <Home /> } />
+				</Routes>
+			</Router>
+	);
+};
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>
 );
